e19e0288b990c017d2fe97b2591da4a662a10bc7
mylo <48256865+Milopadma@users.noreply.github.com>
2022-11-07 21:34:44 +0800

    new impl

--- main.rs
+++ main.rs
@@ -1,14 +1,14 @@
 // all in one CLI tool to check wifi connections (speed, latency, etc.)
 // crate use imports
-use reqwest::blocking::Client;
 use std::time::Instant;
+use reqwest::Client;
 
 // function to check for the speed of the connection by downloading a file 
-fn checkSpeed(){
+async fn checkSpeed: f64 () {
 
     // establish a connection to the server
-    let client = reqwest::blocking::Client::new();
-    let mut response = client.get("https://cachefly.cachefly.net/100mb.test").send().unwrap();
+    let client = Client::new();
+    let mut response = client.get("https://cachefly.cachefly.net/100mb.test").send().await.unwrap();
 
     // check the status code
     // if the status code is not 200, then the connection failed
@@ -23,8 +23,8 @@ fn checkSpeed(){
 
     // get the time it took to download the failed
     let start = Instant::now();
-    let mut buf = Vec::new();
-    response.copy_to(&mut buf).unwrap();
+    let mut buf: Vec<T> = Vec::new();
+
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
