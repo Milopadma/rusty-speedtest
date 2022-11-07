c59bb52482e6d8f9d6cd003f9004b2197fa85cbc
mylo <48256865+Milopadma@users.noreply.github.com>
2022-11-07 23:20:28 +0800

    some progres

--- main.rs
+++ main.rs
@@ -2,13 +2,21 @@
 // crate use imports
 use std::time::Instant;
 use reqwest::Client;
+use tokio;
+
+// show how to implement a Type
+struct T {
+    name: String,
+    speed: u32,
+    latency: u32,
+}
 
 // function to check for the speed of the connection by downloading a file 
-async fn checkSpeed: f64 () {
+async fn check_speed<T>() {
 
     // establish a connection to the server
     let client = Client::new();
-    let mut response = client.get("https://cachefly.cachefly.net/100mb.test").send().await.unwrap();
+    let  response = client.get("https://cachefly.cachefly.net/100mb.test").send().await.unwrap();
 
     // check the status code
     // if the status code is not 200, then the connection failed
@@ -23,14 +31,16 @@ async fn checkSpeed: f64 () {
 
     // get the time it took to download the failed
     let start = Instant::now();
-    let mut buf: Vec<T> = Vec::new();
-
+    let _ = response.bytes().await.unwrap();
     let duration = start.elapsed();
+    
     println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
 
-
-fn main(){
-    
+//macro to make this main function async
+#[tokio::main]
+async fn main(){
+    // call the function
+    check_speed::<T>().await; 
     
 }
