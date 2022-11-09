568eb1741765f7f9288d4d418613bfd9442cfe1c
mylo <48256865+Milopadma@users.noreply.github.com>
2022-11-09 18:47:40 +0800

    something

--- main.rs
+++ main.rs
@@ -11,32 +11,76 @@ struct T {
     latency: u32,
 }
 
-// function to check for the speed of the connection by downloading a file 
+// function to check for the speed of the connection by downloading a file
 async fn check_speed<T>() {
-
     // get the time it took to download the file
     let start = Instant::now();
     // establish a connection to the server
     let client = Client::new();
-    let  response = client.get("https://cachefly.cachefly.net/1000mb.test").send().await.unwrap();
+    let response = client.get("https://cachefly.cachefly.net/1000mb.test").send().await.unwrap();
     if response.status() != 200 {
         println!("Connection failed");
-    }
-
-    // if the status code is 200, then the connection was successful
-    else {
+    } else {
+        // if the status code is 200, then the connection was successful
         println!("Connection successful");
     }
     let _ = response.bytes().await.unwrap();
     let duration = start.elapsed();
-    
+
     println!("Time elapsed in expensive_function() is: {:?}", duration);
+}
+
+//function to check for default gateways
+async fn check_gateway() {
+    // ping each gateway and check if it is reachable
+    let gateway = "
+        192.168.1.20
+        ";
+    let client = Client::new();
+    let response = client.get(gateway).send().await;
+    // pattern matching for the response
+    match response {
+        Ok(response) => {
+            if response.status() != 200 {
+                println!("Connection failed");
+            } else {
+                // if the status code is 200, then the connection was successful
+                println!("Connection successful");
+            }
+        }
+        Err(_) => {
+            println!("Connection failed");
+        }
+    }
+    
+}
+
+// for the CLI selection menu
+enum Menu {
+    Speed,
+    Gateway,
+    Exit,
 }
 
 //macro to make this main function async
 #[tokio::main]
-async fn main(){
-    // call the function
-    check_speed::<T>().await; 
-    
+async fn main() {
+
+    // menu selection
+    println!("Select an option:");
+    println!("1. Check speed");
+    println!("2. Check gateway");
+    println!("3. Exit");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input: u32 = input.trim().parse().unwrap();
+
+    // match the input to the enum
+    match input {
+        // 1 => check_speed().await,
+        2 => check_gateway().await,
+        3 => println!("Exiting..."),
+        _ => println!("Invalid input"),
+    }
+
 }
